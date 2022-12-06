39c777ad007108e265d1ffa4911a4c797d685d96
Atithi Kumar Singh <coder_ravan@pop-os.localdomain>
2022-12-06 12:53:51 +0530

    removed \n from snippet

--- src/components/SecretPage.jsx
+++ src/components/SecretPage.jsx
@@ -55,7 +55,6 @@ function SecretPage() {
 #define rep(i, j) loop(i, 0, j)
 #define rrep(i, j) rloop(i, j, 0)
 #define MP make_pair
-#define endl "\n"
 #define INF (int)1e18
 #define EPS 1e-18
 #define PI 3.1415926535897932384626433832795
@@ -69,88 +68,7 @@ function SecretPage() {
 using namespace std;
 
 /*-----------------------------------D-E-B-U-G-----------------------------------------------*/
-#ifndef ONLINE_JUDGE
-#define deb(x)             \
-        cerr << #x << " "; \
-        _print(x);         \
-        cerr << endl;
-#else
-#define deb(x)
-#endif
 
-void _print(int t)
-{
-        cerr << t;
-}
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(long double t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(unsigned long long t) { cerr << t; }
-
-template <class T, class V>
-void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
-template <class T>
-void _print(multiset<T> v);
-template <class T, class V>
-void _print(pair<T, V> p)
-{
-        cerr << "{";
-        _print(p.first);
-        cerr << ",";
-        _print(p.second);
-        cerr << "}";
-}
-template <class T>
-void _print(vector<T> v)
-{
-        cerr << "[ ";
-        for (T i : v)
-        {
-                _print(i);
-                cerr << " ";
-        }
-        cerr << "]";
-}
-template <class T>
-void _print(set<T> v)
-{
-        cerr << "[ ";
-        for (T i : v)
-        {
-                _print(i);
-                cerr << " ";
-        }
-        cerr << "]";
-}
-template <class T>
-void _print(multiset<T> v)
-{
-        cerr << "[ ";
-        for (T i : v)
-        {
-                _print(i);
-                cerr << " ";
-        }
-        cerr << "]";
-}
-template <class T, class V>
-void _print(map<T, V> v)
-{
-        cerr << "[ ";
-        for (auto i : v)
-        {
-                _print(i);
-                cerr << " ";
-        }
-        cerr << "]";
-}
 /*-----------------------------------D-E-B-U-G-----------------------------------------------*/
 
 void solve()
@@ -165,7 +83,7 @@ signed main()
         int t = 1;
         // cin >> t;
         srand(time(0));
-        cout << t << "\n";
+        cout << t << "endl";
         while (t--)
         {
                 solve();
